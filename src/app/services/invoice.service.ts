5dcc170a7a172e916c9b22c6af61fb61b09e2878
Fescobar28 <franescobar97@hotmail.com>
2025-01-28 14:01:47 -0600

    Se agrega la funcion calcular total

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -6,12 +6,27 @@ import { invoiceData } from '../data/invoiceData';
   providedIn: 'root'
 })
 export class InvoiceService {
-  
+
   private invoice: Invoice = invoiceData;
 
   constructor() { }
 
   getInvoice(): Invoice {
-    return this.invoice;
+    const total = this.calculateTotal()
+    return {... this.invoice, total};
+  }
+
+  // calculateTotal(){
+  //   let total = 0;
+
+  //   this.invoice.items.forEach(item => {
+  //     total = total + item.total();
+  //   })
+
+  //   return total
+  // }
+
+  calculateTotal() {
+    return this.invoice.items.reduce((total, item) => total + (item.total()), 0)
   }
 }
